8acac73dc9a3d8dc329237f835958bacae3598d6
kayne10 <troy.kayne@colorado.edu>
2020-09-05 01:13:42 -0600

    Python shell AWS Glue job succeeded

--- aws-glue/python-shell-jobs/glue_import_orders.py
+++ aws-glue/python-shell-jobs/glue_import_orders.py
@@ -1,15 +1,13 @@
-import boto3,json 
-from pg import DB 
+import boto3
+import json
+from pg import DB #dependency avaiable in python shell jobs
 
-secret_name = 'your-secret-name'
-region_name ='eu-west-1'
+secret_name = 'rs-credentials'
+region_name = 'us-west-2'
 
 session = boto3.session.Session()
-
 client = session.client(service_name='secretsmanager',region_name=region_name)
-
 get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-
 creds = json.loads(get_secret_value_response['SecretString'])
 
 username = creds['username']
@@ -19,25 +17,65 @@ host = creds['host']
 db = DB(dbname='dev',host=host,port=5439,user=username,passwd=password)
 
 merge_qry = """
-			begin ; 
+	begin;
+		copy mysql_dwh_staging.orders from 's3://troy-de-course/orders/current/orders.csv'
+		iam_role 'arn:aws:iam::387932593219:role/myredshift'
+		CSV QUOTE '\"' DELIMITER ','
+		acceptinvchars;
 
-			copy mysql_dwh_staging.orders from 's3://bucket-name/orders/current/orders.csv'
-			iam_role 'YOUR_ARN'
-			CSV QUOTE '\"' DELIMITER ','
-			acceptinvchars;
+		delete from mysql_dwh.orders
+		using mysql_dwh_staging.orders
+		where mysql_dwh.orders.order_id = mysql_dwh_staging.orders.order_id;
+		
+		insert into mysql_dwh.orders select * from mysql_dwh_staging.orders;
 
-			delete 
-				from 
-					mysql_dwh.orders 
-				using mysql_dwh_staging.orders 
-				where mysql_dwh.orders.order_id = mysql_dwh_staging.orders.order_id ;
-
-			insert into mysql_dwh.orders select * from mysql_dwh_staging.orders;
-
-			truncate table mysql_dwh_staging.orders;
-			end ; 
-
-			"""
+		truncate table mysql_dwh_staging.orders;
+	end;
+"""
 
 result = db.query(merge_qry)
 print(result)
+
+# import boto3,json 
+# from pg import DB 
+
+# secret_name = 'your-secret-name'
+# region_name ='eu-west-1'
+
+# session = boto3.session.Session()
+
+# client = session.client(service_name='secretsmanager',region_name=region_name)
+
+# get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+
+# creds = json.loads(get_secret_value_response['SecretString'])
+
+# username = creds['username']
+# password = creds['password']
+# host = creds['host']
+
+# db = DB(dbname='dev',host=host,port=5439,user=username,passwd=password)
+
+# merge_qry = """
+# 			begin ; 
+
+# 			copy mysql_dwh_staging.orders from 's3://bucket-name/orders/current/orders.csv'
+# 			iam_role 'YOUR_ARN'
+# 			CSV QUOTE '\"' DELIMITER ','
+# 			acceptinvchars;
+
+# 			delete 
+# 				from 
+# 					mysql_dwh.orders 
+# 				using mysql_dwh_staging.orders 
+# 				where mysql_dwh.orders.order_id = mysql_dwh_staging.orders.order_id ;
+
+# 			insert into mysql_dwh.orders select * from mysql_dwh_staging.orders;
+
+# 			truncate table mysql_dwh_staging.orders;
+# 			end ; 
+
+# 			"""
+
+# result = db.query(merge_qry)
+# print(result)
